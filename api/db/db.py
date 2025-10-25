4d68689d93e64ff6159d3a03324f3ede0d4813a5
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2025-10-25 14:44:23 +0200

    remove print

--- api/db/db.py
+++ api/db/db.py
@@ -10,8 +10,6 @@ DATABASE_URL = (
     f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 )
 
-print(f"[LOG] DATABASE_URL: {DATABASE_URL}")
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
