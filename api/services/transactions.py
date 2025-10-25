69e2ce07ceea6121e4249f2ac2c39d36b6055b4c
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2025-10-25 15:30:16 +0200

    link get_transaction_by_id to db

--- api/services/transactions.py
+++ api/services/transactions.py
@@ -1,45 +1,8 @@
-from datetime import datetime
 from typing import List
 import uuid
 from api.db.db import SessionLocal
 from api.models.transactions import Transaction
 from api.schemas.transactions import TransactionORM
-
-transactions = [
-    Transaction(
-        id=uuid.UUID("16fd2706-8baf-433b-82eb-8c7fada847da"),
-        asset="BTC",
-        operation="buy",
-        amount=500.0,
-        quantity=0.01,
-        unit_price=50000.0,
-        currency="EUR",
-        date=datetime(2024, 10, 1, 14, 30),
-        comment="First BTC buy",
-    ),
-    Transaction(
-        id=uuid.uuid4(),
-        asset="ETH",
-        operation="buy",
-        amount=300.0,
-        quantity=0.1,
-        unit_price=3000.0,
-        currency="EUR",
-        date=datetime(2024, 10, 3, 10, 0),
-        comment="ETH position",
-    ),
-    Transaction(
-        id=uuid.uuid4(),
-        asset="BTC",
-        operation="sell",
-        amount=250.0,
-        quantity=0.005,
-        unit_price=50000.0,
-        currency="EUR",
-        date=datetime(2024, 10, 10, 16, 45),
-        comment="Partial BTC sale",
-    ),
-]
 
 
 async def get_all_transactions() -> List[Transaction]:
@@ -54,10 +17,16 @@ async def get_all_transactions() -> List[Transaction]:
 
 async def get_transaction_by_id(transaction_id: uuid.UUID) -> Transaction:
     print(f"[TRANSACTION] GET BY ID {transaction_id}")
-    for tx in transactions:
-        if tx.id == transaction_id:
-            return tx
-    return None
+    db = SessionLocal()
+    try:
+        tx = (
+            db.query(TransactionORM).filter(TransactionORM.id == transaction_id).first()
+        )
+        if not tx:
+            return False
+        return Transaction.model_validate(tx)
+    finally:
+        db.close()
 
 
 async def create_one_transaction(transaction: Transaction) -> Transaction:
