b9086bf3e3d1c6f0f983fb6d223b551791f92578
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2025-11-05 22:30:48 +0100

    update env var

--- api/db/db.py
+++ api/db/db.py
@@ -5,11 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
-    f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
-)
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(os.getenv("DB_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
