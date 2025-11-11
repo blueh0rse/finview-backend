e8fb6d0d841dfaa8c135970b4424441f92dee42d
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2025-11-11 21:54:05 +0100

    add get /portefolio/allocation route + fix naming

--- api/routes/portefolio.py
+++ api/routes/portefolio.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter
-from api.services.portefolio import get_portefolio_value
+from api.services.portefolio import calc_portefolio_value, calc_portfolio_allocation
 
-router = APIRouter(tags=["Transactions"])
+router = APIRouter(tags=["Portefolio"])
 
 
 @router.get("/portefolio/value")
-async def get_transactions():
+async def get_portfolio_value():
     """Retrieve portefolio value"""
     print("[REQ] GET /portefolio/value")
-    value = await get_portefolio_value()
+    value = await calc_portefolio_value()
     return {"value": value}
+
+
+@router.get("/portefolio/allocation")
+async def get_portfolio_allocation():
+    """Retrieve portefolio allocation"""
+    print("[REQ] GET /portefolio/allocation")
+    total_value, allocation = await calc_portfolio_allocation()
+    return {"total_value": total_value, "allocation": allocation}
--- api/services/portefolio.py
+++ api/services/portefolio.py
@@ -1,11 +1,9 @@
-from sqlalchemy import func
+from sqlalchemy import case, func
 from api.db.db import SessionLocal
 from api.schemas.transactions import OperationEnum, TransactionORM
 
-MAX_TRANSACTIONS = 100
 
-
-async def get_portefolio_value() -> float:
+async def calc_portefolio_value() -> float:
     print("[PORTEFOLIO] CALC VALUE")
     db = SessionLocal()
     try:
@@ -24,5 +22,50 @@ async def get_portefolio_value() -> float:
 
         total_value = buy_total - sell_total
         return float(total_value)
+    finally:
+        db.close()
+
+
+async def calc_portfolio_allocation():
+    print("[PORTEFOLIO] CALC ALLOCATION")
+    db = SessionLocal()
+    try:
+        # net quantity
+        results = (
+            db.query(
+                TransactionORM.asset,
+                func.sum(
+                    case(
+                        (
+                            TransactionORM.operation == OperationEnum.BUY.value,
+                            TransactionORM.quantity,
+                        ),
+                        (
+                            TransactionORM.operation == OperationEnum.SELL.value,
+                            -TransactionORM.quantity,
+                        ),
+                        else_=0,
+                    )
+                ).label("net_quantity"),
+                func.avg(TransactionORM.unit_price).label("avg_unit_price"),
+            )
+            .group_by(TransactionORM.asset)
+            .all()
+        )
+
+        # total value per asset
+        allocation = []
+        total_value = 0
+        for asset, qty, avg_price in results:
+            if qty and qty > 0:
+                value = qty * avg_price
+                total_value += value
+                allocation.append({"asset": asset, "value": value})
+
+        # calculate percentages
+        for a in allocation:
+            a["percent"] = round(a["value"] / total_value * 100, 2)
+
+        return total_value, allocation
     finally:
         db.close()
