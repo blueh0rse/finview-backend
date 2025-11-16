707823126c9ac027004634aef8d6791fd1d96f42
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2025-11-16 21:34:24 +0100

    fix model import

---
+++ api/db/models.py
@@ -0,0 +1,2 @@
+from api.schemas.transactions import TransactionORM
+from api.schemas.assets import AssetORM
--- migrations/env.py
+++ migrations/env.py
@@ -3,7 +3,8 @@ import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from api.schemas.transactions import Base
+from api.db.db import Base
+import api.db.models
 
 from alembic import context
 
