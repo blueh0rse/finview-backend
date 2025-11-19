28f55e3947b069ae549969aaf8ade3ff4c4fa828
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2025-11-19 21:27:48 +0100

    ensure asset symbol is unique

--- api/schemas/assets.py
+++ api/schemas/assets.py
@@ -8,7 +8,7 @@ class AssetORM(Base):
     __tablename__ = "assets"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    symbol = Column(String, nullable=False)
+    symbol = Column(String, nullable=False, unique=True)
     name = Column(String, nullable=False)
     category = Column(String, nullable=False)
     current_price = Column(Float, nullable=True)
